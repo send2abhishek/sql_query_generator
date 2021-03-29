a731bf60bc277069d308090e125ac246c2f50994
Kumar, Abhishek <abhishek.kumar3@tyson.com>
2021-03-29 08:25:51 +0530

    reading multiple columns in excel

--- index.js
+++ index.js
@@ -9,23 +9,30 @@ const firstSheet = worksheet[0];
 // select the sheet which you want to read
 const selectedSheet = workbook.Sheets[firstSheet];
 
+// console.log("selectedSheet", selectedSheet);
 let supplier = [];
 
-Object.keys(selectedSheet).forEach((sheet) => {
-  if (selectedSheet[sheet].v !== undefined) {
+Object.keys(selectedSheet).forEach((sheet, index) => {
+  if (
+    selectedSheet[`A${index + 1}`] !== undefined &&
+    selectedSheet[`B${index + 1}`] !== undefined &&
+    selectedSheet[`C${index + 1}`] !== undefined
+  ) {
     supplier.push({
-      supplier: selectedSheet[sheet].v,
+      name: selectedSheet[`A${index + 1}`].v,
+      category: selectedSheet[`B${index + 1}`].v,
+      lag: selectedSheet[`C${index + 1}`].v,
     });
   }
 });
+console.log("supplier", supplier);
+// var stream = fs.createWriteStream("query.txt", { flags: "a" });
 
-var stream = fs.createWriteStream("query.txt", { flags: "a" });
+// supplier.forEach((element, index) => {
+//   let sql = `INSERT INTO Dim_Normalized_Supplier  VALUES (${index + 1},'${
+//     element.supplier
+//   }','Kumar, Abhishek','abhishek.kumar3@tyson.com','2021-03-26 19:02:58','2021-03-26 19:02:58');`;
 
-supplier.forEach((element, index) => {
-  let sql = `INSERT INTO Dim_Normalized_Supplier  VALUES (${index + 1},'${
-    element.supplier
-  }','Kumar, Abhishek','abhishek.kumar3@tyson.com','2021-03-26 19:02:58','2021-03-26 19:02:58');`;
-
-  stream.write(sql + "\n");
-});
-stream.end();
+//   stream.write(sql + "\n");
+// });
+// stream.end();
